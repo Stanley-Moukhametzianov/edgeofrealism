56a66b28bde4ba31ca7b1d2efc7894c3d0e19350
Stanley Moukhametzianov <stanleymoukh@gmail.com>
2023-05-18 23:50:44 -0400

    testing

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,55 +1,14 @@
-const puppeteer = require('puppeteer');
+const chromium = require('chrome-aws-lambda');
 
 
 
 const fetchData = async(searchTerm) => {
-    const browser = await puppeteer.launch({
-      headless: false,
-      args: [
-        '--disable-accelerated-2d-canvas',
-        '--single-process',
-        '--disable-gpu',
-        '--autoplay-policy=user-gesture-required',
-        '--disable-background-networking',
-        '--disable-background-timer-throttling',
-        '--disable-backgrounding-occluded-windows',
-        '--disable-breakpad',
-        '--disable-client-side-phishing-detection',
-        '--disable-component-update',
-        '--disable-default-apps',
-        '--disable-dev-shm-usage',
-        '--disable-domain-reliability',
-        '--disable-extensions',
-        '--disable-features=AudioServiceOutOfProcess',
-        '--disable-hang-monitor',
-        '--disable-ipc-flooding-protection',
-        '--disable-notifications',
-        '--disable-offer-store-unmasked-wallet-cards',
-        '--disable-popup-blocking',
-        '--disable-print-preview',
-        '--disable-prompt-on-repost',
-        '--disable-renderer-backgrounding',
-        '--disable-setuid-sandbox',
-        '--disable-speech-api',
-        '--disable-sync',
-        '--hide-scrollbars',
-        '--ignore-gpu-blacklist',
-        '--metrics-recording-only',
-        '--mute-audio',
-        '--no-default-browser-check',
-        '--no-first-run',
-        '--no-pings',
-        '--no-sandbox',
-        '--no-zygote',
-        '--password-store=basic',
-        '--use-gl=swiftshader',
-        '--use-mock-keychain',
-        '--window-size=1920,1080',
-        '--headless'
-      ],
-
-      defaultViewport: null,
-
+    const browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
     });
 
     const page = (await browser.pages())[0];
