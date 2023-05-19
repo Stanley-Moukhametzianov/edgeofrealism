34c7ede8b2767744e8d84cecff5389498df6f13c
Stanley Moukhametzianov <stanleymoukh@gmail.com>
2023-05-19 13:02:51 -0400

    test

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -13,63 +13,15 @@ const fetchData = async(searchTerm) => {
 
     const page = (await browser.pages())[0];
 
-
-
-    // Array of third-party domains to block
-    const blockedDomains = [
-      'https://pagead2.googlesyndication.com',
-      'https://creativecdn.com',
-      'https://www.googletagmanager.com',
-      'https://cdn.krxd.net',
-      'https://adservice.google.com',
-      'https://cdn.concert.io',
-      'https://z.moatads.com',
-      'https://cdn.permutive.com',
-      'https://googlesyndication.com',
-    ];
-    await page.setRequestInterception(true);
-
-
-    page.on('request', (request) => {
-      const url = request.url();
-      if (blockedDomains.some((d) => url.startsWith(d))) {
-        request.abort();
-      } else {
-        request.continue();
-      }
-    });
-
     //End blocking domains
 
     await page.goto('https://dream.ai/create');
 
-    await page.waitForXPath('//*[@id="blur-overlay"]/div/div[2]/div[1]/div[1]/div/div[2]/div[1]/div[1]/input');
-
-
-    await page.type('input', searchTerm);
-
-    /* const returnArr = [];
-
-    for (var i = 0; i < 3; i++) { */
-
-      const elements = await page.$x('//*[@id="blur-overlay"]/div/div[2]/div[1]/div[2]/button');
-      await elements[0].click();
-
-
-      await page.waitForXPath('//*[@id="blur-overlay"]/div/div[2]/div[2]/div[1]/div/div/div[1]/div/div/div/div[4]/img')
-
-      const returnObj = await page.evaluate(() => {
-        const el = document.querySelectorAll('img');
-        return el[el.length - 1].src;
-
-      });
-
-     /*  returnArr.push(returnObj);
-    } */
+  
 
   await browser.close();
   
-    return returnObj;
+    return 'done';
 }
 
 
