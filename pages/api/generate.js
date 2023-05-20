f5af4e3e3f18a099c838ea27145d024a3b92b936
Stanley Moukhametzianov <stanleymoukh@gmail.com>
2023-05-20 12:20:47 -0400

    finally somethings working

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -7,22 +7,78 @@ const LOCAL_CHROME_EXECUTABLE = '/Applications/Google Chrome.app/Contents/MacOS/
 
 const fetchData = async (searchTerm) => {
 
+  var returnObj = '';
+
     try {
 
   const executablePath = await edgeChromium.executablePath || LOCAL_CHROME_EXECUTABLE
 
-  console.log('1');
 
   const browser = await puppeteer.launch({
     executablePath,
-    args: edgeChromium.args,
+    args: [
+      ...edgeChromium.args,
+      '--window-size=1920,1080',
+        '--headless'
+      ]
+    ,
     headless: false,
+    defaultViewport: null,
   })
 
-  console.log('2');
 
-  const page = await browser.newPage()
-  await page.goto('https://github.com')
+      const page = (await browser.pages())[0];
+
+
+
+      // Array of third-party domains to block
+      const blockedDomains = [
+        'https://pagead2.googlesyndication.com',
+        'https://creativecdn.com',
+        'https://www.googletagmanager.com',
+        'https://cdn.krxd.net',
+        'https://adservice.google.com',
+        'https://cdn.concert.io',
+        'https://z.moatads.com',
+        'https://cdn.permutive.com',
+        'https://googlesyndication.com',
+      ];
+      await page.setRequestInterception(true);
+
+
+      page.on('request', (request) => {
+        const url = request.url();
+        if (blockedDomains.some((d) => url.startsWith(d))) {
+          request.abort();
+        } else {
+          request.continue();
+        }
+      });
+
+      //End blocking domains
+
+      await page.goto('https://dream.ai/create');
+
+      await page.waitForXPath('//*[@id="blur-overlay"]/div/div[2]/div[1]/div[1]/div/div[2]/div[1]/div[1]/input');
+
+
+      await page.type('input', 'Rain');
+
+
+
+        const elements = await page.$x('//*[@id="blur-overlay"]/div/div[2]/div[1]/div[2]/button');
+        await elements[0].click();
+
+
+        await page.waitForXPath('//*[@id="blur-overlay"]/div/div[2]/div[2]/div[1]/div/div/div[1]/div/div/div/div[4]/img')
+
+        returnObj = await page.evaluate(() => {
+          const el = document.querySelectorAll('img');
+          return el[el.length - 1].src;
+
+        });
+
+      console.log(returnObj);
 
       await browser.close();
       
@@ -31,7 +87,7 @@ const fetchData = async (searchTerm) => {
       console.log(e);
     }
 
-  return 'Done';
+  return returnObj;
 }
 
 
